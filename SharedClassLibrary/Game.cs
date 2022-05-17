a35b5d5dfc1e792f0b08a63d58ff91de5c13cb41
Mo-ismaiil <98978939+Mo-ismaiil@users.noreply.github.com>
2022-05-17 12:02:47 +0200

    Add files via upload

---
+++ SharedClassLibrary/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SimpleServer.ClassLib
+{
+	[Serializable]
+	public class Game
+	{
+		public int[][] BoardData { get; set; }
+		public int Board1X { get; set; }
+		public int Board2X { get; set; }
+		public int Board3X { get; set; }
+		public int Board4X { get; set; }
+		public int Board5X { get; set; }
+		public int Board6X { get; set; }
+		public int Board7X { get; set; }
+
+		public Game()
+		{
+			BoardData = new int[6][];//for 6*7 and 4*5
+			BoardData[0] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+			BoardData[1] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+			BoardData[2] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+			BoardData[3] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+			BoardData[4] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+			BoardData[5] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+		}
+
+		public void ResetTheBoard()
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					BoardData[i][j] = 0;
+				}
+			}
+		}
+	}
+}
---
+++ SharedClassLibrary/Room.cs
@@ -0,0 +1,20 @@
+﻿using SimpleServer.ClassLib;
+using System;
+using System.Collections.Generic;
+
+namespace ShardClassLibrary
+{
+	[Serializable]
+	public class Room
+	{
+		public int Index { get; set; }
+		public string RoomName { get; set; }
+		public string Status { get; set; }
+		public User RoomOwner { get; set; }
+		public string RoomOwnerDiskColor { get; set; }
+		public string RoomBoardSize { get; set; }
+		public List<User> Players { get; set; }
+		public List<User> Spectators { get; set; }
+		public Game Game { get; set; }
+	}
+}
